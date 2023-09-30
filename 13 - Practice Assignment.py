cbe6742eb5ac98c2163a6db9aaad64f21286bef5
Daniel White <dwhite317@gatech.edu>
2023-09-29 17:01:41 -0700

    Fixed some descriptions

--- 13 - Practice Assignment.py
+++ 13 - Practice Assignment.py
@@ -23,7 +23,7 @@ def even_numbers(num):
 # When you are ready to test your fuction, save your code and run the script. The output will show if you passed or failed each of the tests.
 # Your goal is to pass all of the tests.
 
-# These are test cases which will run agaist your code
+# These are test cases which will run against your code
 cases = [2, 5, 20, 41, 50, 71]
 # Wow, you can put lists inside of lists? That's pretty cool!
 answers = [
@@ -34,6 +34,8 @@ answers = [
     [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48],
     [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70]
 ]
+
+# This will run the test cases and see if the correct answer is found.
 
 for i in range(len(cases)):
     current_case = cases[i]
